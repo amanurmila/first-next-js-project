6a57f125662e1e680a9ea329d6c42acd955d1a5d
Md Amanullah <muhammadamanullah6001@gmail.ccom>
2025-01-24 17:11:09 +0600

    fixed some sisue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Set-Cookie",
+            value: "SameSite=Lax; Secure",
+          },
+        ],
+      },
+    ];
+  },
+};
